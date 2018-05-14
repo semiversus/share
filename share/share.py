a88b20da22badc93feab5ff06f8fa030ec295fcf
Günther Jena <guenther@jena.at>
2018-05-15 01:15:39 +0200

    wip: added add_item.html

--- share/share.py
+++ share/share.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add', methods=['POST', 'GET'])
+def add():
+    if request.method == 'POST':
+        return render_template('index.html')
+    else:
+        return render_template('add_item.html')
+
+if __name__ == '__main__':
+    app.run()
